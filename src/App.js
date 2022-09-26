24b711359f02da21754b9e5364049efa952742bc
Emily Pisaygnane <episaygnane@me.com>
2022-09-26 14:59:30 -0700

    added new route in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import BookList from './components/book/BookList';
 import BookDetail from './components/book/BookDetail';
 import { Switch, Route } from 'react-router-dom';
-
 import './App.css';
 
 function App() {
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <Switch>
-        <Route path="/books/:id">
-          <BookDetail />
-        </Route>
+    <Switch>
+      <Route exact path="/">
+        <main className="container">
+          <h1>Library Catalog</h1>
+        </main>
+      </Route>
 
-        <Route path="/books">
-          <BookList />
-        </Route>
-      </Switch>
-    </main>
+      <Route path="/books/:id">
+        <BookDetail />
+      </Route>
+
+      <Route path="/books">
+        <BookList />
+      </Route>
+    </Switch>
   );
 }
 
