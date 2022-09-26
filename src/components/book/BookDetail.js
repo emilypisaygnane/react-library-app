271e6dc67ae98795cf008ba02921cf81e3f28185
Emily Pisaygnane <episaygnane@me.com>
2022-09-26 13:20:43 -0700

    added return link from book detail to book list

--- src/components/book/BookDetail.js
+++ src/components/book/BookDetail.js
@@ -1,6 +1,6 @@
 import Book from './Book';
 import { useBookDetail } from '../../hooks/useBookDetail';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 function BookDetail() {
   const { id } = useParams();
@@ -15,7 +15,12 @@ function BookDetail() {
 
   if (loading) return <h3>Loading book...</h3>;
 
-  return <Book book={book} showDetail />;
+  return (
+    <div>
+      <Link to={`/books`}>Return to Book List</Link>
+      <Book book={book} showDetail />
+    </div>
+  );
 }
 
 export default BookDetail;
