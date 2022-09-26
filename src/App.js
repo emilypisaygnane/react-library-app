f58eb3f4539b87c384540aa2a61e4feb428a8704
Emily Pisaygnane <episaygnane@me.com>
2022-09-26 13:02:45 -0700

    imported BookDetail and pathed /books and /books/:id in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import BookList from './components/book/BookList';
+import BookDetail from './components/book/BookDetail';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   // TODO: Add routes to books & views
+
+  <Switch>
+    <Route path='/books' component={BookList} />
+    <Route path="/books/:id" component={BookDetail} />
+  </Switch>;
+  
   return (
     <main className="container">
       <h1>Library Catalog</h1>
